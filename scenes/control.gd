236d60ee11fd5f3e1dbb3158282530e68f80cc0f
AydinHumphries <48067862+AydinHumphries@users.noreply.github.com>
2020-09-20 16:03:29 +0100

    Add files via upload

---
+++ scenes/control.gd
@@ -0,0 +1,16 @@
+extends Control
+var socket
+
+func _ready():
+	socket = StreamPeerTCP.new()
+	$buttonConnect.connect("pressed",self,"_on_connect")
+	$buttonMessage.connect("pressed",self,"_on_message")
+
+func _on_connect():
+	socket.connect_to_host("127.0.0.1", 4242)
+
+func _on_message():
+	socket.put_data("myMessage;".to_ascii())
+
+func _on_message2():
+		socket.put_data("myMessage2; banana;".to_ascii())
